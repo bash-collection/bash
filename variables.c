1a21dfe8edb4bda704bbf7c505b6d5761414f48a
Koichi Murase <myoga.murase@gmail.com>
2026-08-13 05:25:30 +0900

    perf(all_variables_matching_prefix): avoid making list of all visible variables

--- variables.c
+++ variables.c
@@ -4495,26 +4495,37 @@ all_array_variables (void)
 }
 #endif /* ARRAY_VARS */
 
+static struct {
+  const char *prefix;
+  size_t plen;
+} variable_matching_prefix_context;
+
+static int
+variable_matching_prefix (SHELL_VAR *var)
+{
+  const char *prefix = variable_matching_prefix_context.prefix;
+  size_t plen = variable_matching_prefix_context.plen;
+  return (invisible_p (var) == 0 && (plen == 0 || STREQN (prefix, var->name, plen)));
+}
+
 char **
 all_variables_matching_prefix (const char *prefix)
 {
   SHELL_VAR **varlist;
   char **rlist;
   int rind;
-  size_t plen, vind;
+  size_t vind;
 
-  plen = STRLEN (prefix);
-  varlist = all_visible_variables ();
+  variable_matching_prefix_context.prefix = prefix;
+  variable_matching_prefix_context.plen = STRLEN (prefix);
+  varlist = vapply (variable_matching_prefix);
   for (vind = 0; varlist && varlist[vind]; vind++)
     ;
   if (varlist == 0 || vind == 0)
     return ((char **)NULL);
   rlist = strvec_create (vind + 1);
   for (vind = rind = 0; varlist[vind]; vind++)
-    {
-      if (plen == 0 || STREQN (prefix, varlist[vind]->name, plen))
-	rlist[rind++] = savestring (varlist[vind]->name);
-    }
+    rlist[rind++] = savestring (varlist[vind]->name);
   rlist[rind] = (char *)0;
   free (varlist);
 
